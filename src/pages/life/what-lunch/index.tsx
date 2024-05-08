05a80b427a3a7309296a905884c8ba0d394059ae
hankliu <397694072@qq.com>
2024-05-08 08:42:28 +0800

    feat(toolkits): add what lunch page

---
+++ src/pages/life/what-lunch/index.tsx
@@ -0,0 +1,21 @@
+import { ReactElement } from "react";
+import NoHeaderLayout from "@/layouts/no-header";
+
+const LifeWhatLunchPage = () => {
+  return (
+    <iframe
+      title="人生一格"
+      src="https://hankliu62.github.io/what-lunch"
+      width="100%"
+      height="100%"
+      className="h-[100vh] border-0"
+    />
+  );
+};
+
+// 自定义Layout
+LifeWhatLunchPage.getLayout = function getLayout(page: ReactElement) {
+  return <NoHeaderLayout>{page}</NoHeaderLayout>;
+};
+
+export default LifeWhatLunchPage;
