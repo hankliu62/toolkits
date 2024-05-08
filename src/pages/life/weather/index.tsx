0d0667db670b034449c8d442d0791ee7b8277620
hankliu <397694072@qq.com>
2024-05-08 08:34:27 +0800

    feat(toolkits): add weather page

---
+++ src/pages/life/weather/index.tsx
@@ -0,0 +1,21 @@
+import { ReactElement } from "react";
+import NoHeaderLayout from "@/layouts/no-header";
+
+const LifeWeatherPage = () => {
+  return (
+    <iframe
+      title="人生一格"
+      src="https://hankliu62.github.io/weather?with-header=1"
+      width="100%"
+      height="100%"
+      className="h-[100vh] border-0"
+    />
+  );
+};
+
+// 自定义Layout
+LifeWeatherPage.getLayout = function getLayout(page: ReactElement) {
+  return <NoHeaderLayout>{page}</NoHeaderLayout>;
+};
+
+export default LifeWeatherPage;
