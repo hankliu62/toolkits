dd3b6aed0fd997a6392bde88b1cae401fbb3735e
hankliu <397694072@qq.com>
2024-05-08 08:33:42 +0800

    feat(toolkits): add life count page

---
+++ src/pages/life/count/index.tsx
@@ -0,0 +1,22 @@
+// pages/lifecount.js
+import { ReactElement } from "react";
+import NoHeaderLayout from "@/layouts/no-header";
+
+const LifeCountPage = () => {
+  return (
+    <iframe
+      title="人生一格"
+      src="https://hankliu62.github.io/lifecount?with-header=1"
+      width="100%"
+      height="100%"
+      className="h-[100vh] border-0"
+    />
+  );
+};
+
+// 自定义Layout
+LifeCountPage.getLayout = function getLayout(page: ReactElement) {
+  return <NoHeaderLayout>{page}</NoHeaderLayout>;
+};
+
+export default LifeCountPage;
