370aa02ee2515eaef051744b37126a2e61870e25
hankliu <397694072@qq.com>
2024-05-11 08:08:45 +0800

    feat(toolkits): add whiteboard page

---
+++ src/pages/other/whiteboard/index.tsx
@@ -0,0 +1,22 @@
+import { ReactElement } from "react";
+
+import NoHeaderLayout from "@/layouts/no-header";
+
+const OtherWhiteboardPage = () => {
+  return (
+    <iframe
+      title="手绘白板"
+      src="https://hankliu62.github.io/whiteboard"
+      width="100%"
+      height="100%"
+      className="h-[100vh] border-0"
+    />
+  );
+};
+
+// 自定义Layout
+OtherWhiteboardPage.getLayout = function getLayout(page: ReactElement) {
+  return <NoHeaderLayout>{page}</NoHeaderLayout>;
+};
+
+export default OtherWhiteboardPage;
