a03a477a4c6eb5ee5c02ade84b8b714ed7d30b48
hankliu <397694072@qq.com>
2024-05-14 07:00:39 +0800

    feat(toolkits): add pomodoro-technique page

---
+++ src/pages/other/pomodoro-technique/index.tsx
@@ -0,0 +1,27 @@
+import { ReactElement } from "react";
+
+import NoHeaderLayout from "@/layouts/no-header";
+
+/**
+ * 番茄工作法
+ *
+ * @returns
+ */
+const LifePomodoroTechniquePage = () => {
+  return (
+    <iframe
+      title="番茄工作法"
+      src="https://hankliu62.github.io/pomodoro-technique?with-footer=1&with-breadcrumb=1"
+      width="100%"
+      height="100%"
+      className="h-[100vh] border-0"
+    />
+  );
+};
+
+// 自定义Layout
+LifePomodoroTechniquePage.getLayout = function getLayout(page: ReactElement) {
+  return <NoHeaderLayout>{page}</NoHeaderLayout>;
+};
+
+export default LifePomodoroTechniquePage;
