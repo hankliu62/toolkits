eb9bf0c7c3a1ada61c1b6b92c3cd24b7941796c0
hankliu <397694072@qq.com>
2024-05-20 07:55:04 +0800

    feat(toolkits): modify stylelint config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,17 +1,5 @@
+const fabric = require("@hankliu/fabric");
+
 module.exports = {
-  processors: [],
-  plugins: [],
-  extends: [
-    "stylelint-config-standard",
-    "stylelint-order",
-    "stylelint-config-recess-order",
-  ],
-  rules: {
-    "at-rule-no-unknown": [
-      true,
-      {
-        ignoreAtRules: ["tailwind"],
-      },
-    ],
-  }, // 可以自己自定一些规则
+  ...fabric.stylelint,
 };
