f909ef5e1e504f024e4f67a797036e762389065d
hankliu <397694072@qq.com>
2024-05-20 09:59:38 +0800

    feat(toolkits): modify no-header container

--- src/layouts/no-header.tsx
+++ src/layouts/no-header.tsx
@@ -1,34 +1,21 @@
-import dynamic from 'next/dynamic';
-import { useRouter } from 'next/router';
-import type { ReactNode } from 'react';
-
-import useTopWindow from '@/hooks/useTopWindow';
-
-const Footer = dynamic(() => import('@hankliu/rc-footer'), {
-  ssr: false,
-});
+import dynamic from "next/dynamic";
+import type { ReactNode } from "react";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 export default function NoHeaderLayout({ children }: LayoutProps) {
-  const router = useRouter();
-  const isTop = useTopWindow();
-
   return (
     <>
       <div
         className="flex h-full min-h-[100vh] w-full flex-col"
-        style={{ flexDirection: 'column', minHeight: '100vh' }}
+        style={{ flexDirection: "column", minHeight: "100vh" }}
       >
         {/* Main content */}
         <main className="flex flex-1 grow-[1] flex-col" style={{ flex: 1 }}>
           {children}
         </main>
-
-        {/* footer */}
-        {!!(isTop || router.query?.['with-footer']) && <Footer />}
       </div>
     </>
   );
